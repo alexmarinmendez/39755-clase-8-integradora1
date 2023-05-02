3b111373e9ccee80d3b5a279ecb1a3563e510628
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-02 18:01:52 -0500

    feat: afterclass PUT and DELETE pokemon

--- src/routes/pokemon.router.js
+++ src/routes/pokemon.router.js
@@ -5,13 +5,19 @@ const router = Router()
 router.get('/', async (req, res) => {
     // res.send('Listando pokemons...')
     const pokemons = await pokeModel.find().lean().exec()
-    console.log(pokemons)
+    // console.log(pokemons)
     res.render('list', { pokemons })
 })
 
 // /pokemon/create
 router.get('/create', (req, res) => {
     res.render('create', {})
+})
+
+router.get('/update/:name', async (req, res) => {
+    const name = req.params.name
+    const pokemon = await pokeModel.findOne({ name }).lean().exec()
+    res.render('update', { pokemon })
 })
 
 router.get('/:name', async (req, res) => {
@@ -30,9 +36,27 @@ router.post('/', async (req, res) => {
     res.redirect(`/pokemon/${pokemonGenerated.name}`)
 })
 
-router.delete('/:name', (req, res) => {
+router.put('/:name', async (req, res) => {
     const name = req.params.name
-    res.send(`Borrando pokemon ${name}`)
+    console.log(name)
+    const pokemonNewData = req.body
+    console.log(pokemonNewData)
+    try {
+        await pokeModel.updateOne({ name }, { ...pokemonNewData })
+    } catch(err) {
+        console.log('error.....')
+        res.send({err})
+    }
+})
+
+router.delete('/:name', async (req, res) => {
+    const name = req.params.name
+    try {
+        await pokeModel.deleteOne({ name })
+        res.send(`Pokemon ${name} borrado exitosamente!`)
+    } catch (err) {
+        res.send({err})
+    }
 })
 
 export default router
